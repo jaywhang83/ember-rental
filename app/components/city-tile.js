850d90a0cc64aac148eb97e2ff2479b62e878c20
Jay <jaywhang83@gmail.com>
2016-04-04 14:05:36 -0700

    Add computed for combining two properties and sorting rent by price

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+    return this.get('city.name') + ', ' + this.get('city.country');
+  }),
+
+  sortBy: ['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy')
   updateCityForm: false,
   actions: {
     delete(city) {
