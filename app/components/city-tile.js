957b22a39fba8f4da7bb7719d26350cccb571819
Jay <jaywhang83@gmail.com>
2016-04-04 11:27:50 -0700

    Add city model, save and delete city and retals belong to that city

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateCityForm: false,
+  actions: {
+    delete(city) {
+      if(confirm('Are you sure you want to delete this city?')) {
+        this.sendAction('destroyCity', city);
+      }
+    },
+  }
 });
